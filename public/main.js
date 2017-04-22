27897e99649bcd4c3a7fe5793c95fdbfd1e6ca7a
Cosmaan <kslvkon@gmail.com>
2017-04-22 15:33:21 +0300

    add scoreboadr and join firebase

--- public/main.js
+++ public/main.js
@@ -8,37 +8,76 @@ var config = {
   messagingSenderId: "562207389623"
 };
 firebase.initializeApp(config);
-var database = firebase.database();
 
+
+//Getting database
+const reffer = firebase.database().ref('users');
+
+//const onRef = reffer.child('users');
+//console.log(firebase.database().ref('users/'));
+
+//Input Filter
+Vue.directive("filter", {
+  bind: function(el, binding) {
+    this.inputHandler = function(e) {
+      var ch = String.fromCharCode(e.which);
+      var re = new RegExp(binding.value);
+      if (!ch.match(re)) {
+        e.preventDefault();
+      }
+    };
+    el.addEventListener("keypress", this.inputHandler);
+  },
+  unbind: function(el) {
+    el.removeEventListener("keypress", this.inputHandler);
+  },
+  inputHandler: null
+});
 
 //Initialize Vue app
-
-var app = new Vue({
+let app = new Vue({
   el: '#app',
   data: {
-    inputEmail: '',
-    inputPAssword: '',
+    place: 0,
+    score: 0,
+    nickName: '',
     alertAppeare: false,
-    errorMessage: ''
+    errorMessage: '',
+    showBoard: true,
+    scoreBoard: [],
   },
   methods: {
-    test: function() {
-      this.alertAppeare = !this.alertAppeare;
+    alertOn: function() {
+      this.alertAppeare = false;
+    },
+    upe: function() {
+      this.score += 1;
     },
     login: function() {
-      firebase.auth().signInWithEmailAndPassword(this.inputEmail, this.inputPAssword).catch(function(error) {
-        this.errorMessage = error.message;
-        console.log(this.errorMessage);
-        this.alertAppeare = true;
-      });
+        if (this.nickName == '') {
+          this.errorMessage = 'Invalid nick';
+          this.alertAppeare = true;
+          return;
+        }
+        else {
+          this.alertAppeare = false;
+        }
+        firebase.database().ref('users/'+this.nickName).set(this.score);
     },
-    signUp: function() {
-      firebase.auth().createUserWithEmailAndPassword(this.inputEmail, this.inputPAssword).catch(function(error) {
-        this.errorMessage = error.message;
-        console.log(this.errorMessage);
-        this.alertAppeare = true;
+    osb: function() {
+      this.scoreBoard = [];
+
+      let sb = this.scoreBoard;
+      let listPlace = this.place;
+      reffer.orderByValue().limitToLast(50).once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          listPlace += 1;
+          let currentNick = childSnapshot.key;
+          let currentScore = childSnapshot.val();
+          sb.push(currentNick + ' ' + currentScore);
+        });
+        this.sb = sb.reverse();
       });
     }
-
   }
 })
